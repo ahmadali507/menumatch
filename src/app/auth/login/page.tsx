dad12baaf405e18acdafdbf7a4be0f6aa9b4631b
ahmadali507 <ashahid.bese23seecs@seecs.edu.pk>
2025-02-06 00:56:35 +0500

    feat:forgot password added

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -55,7 +55,7 @@ const Card = styled(MuiCard)(({ theme }) => ({
   }),
 }));
 
-const SignInContainer = styled(Stack)(({ theme }) => ({
+export const SignInContainer = styled(Stack)(({ theme }) => ({
   height: "calc((1 - var(--template-frame-height, 0)) * 100dvh)",
   minHeight: "100%",
   padding: theme.spacing(2),
